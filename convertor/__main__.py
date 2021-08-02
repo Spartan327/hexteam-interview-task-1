e7bd982c14903ad43e33dc753d05f7e64ae73a24
Spartan327 <vbutovchenko@mail.ru>
2021-08-02 07:00:17 +0300

    complete 3 part

--- convertor/__main__.py
+++ convertor/__main__.py
@@ -16,15 +16,20 @@ def parse_args():
     return parser.parse_args()
 
 
+def file_to_dict(filename) -> dict[str, str]:
+    dict_letters = {}
+    with open(filename) as file:
+        for line in file:
+            if line:
+                dict_letters[line[0].lower()] = line[2:].split(',')[0].strip()
+    return dict_letters
+
+
 def decode():
     # use a prepare_text_source function for preparing!
     # TODO: need to implement this functionality [4]
     input_text = prepare_text_source(parse_args().source_text)
-    dict_letters = {}
-    with open('convertor/' + KEY_FILE) as file:
-        for line in file:
-            if line:
-                dict_letters[line[0].lower()] = line[2:].split(',')[0].strip()
+    dict_letters = file_to_dict('convertor/' + KEY_FILE)
     for original, replace in dict_letters.items():
         input_text = input_text.replace(original, replace, -1)
     return print(input_text)
@@ -33,11 +38,7 @@ def decode():
 def encode():
     # TODO: need to implement this functionality [5]
     input_text = parse_args().source_text
-    dict_letters = {}
-    with open('convertor/' + KEY_FILE) as file:
-        for line in file:
-            if line:
-                dict_letters[line[0].lower()] = line[2:].split(',')[0].strip()
+    dict_letters = file_to_dict('convertor/' + KEY_FILE)
     sorted_keys_len_values = sorted(
             dict_letters,
             key=lambda k: len(dict_letters[k]),
